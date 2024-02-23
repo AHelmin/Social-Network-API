b1c5d5ea000e8c63290314fa54fefc205e895e7f
helm0127 <helm0127@morris.umn.edu>
2024-02-22 18:27:07 -0600

    tentatively completed models

--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,95 @@
 const mongoose = require('mongoose');
 
-const thoughtSchema = new mongoose.Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        minLength: 1,
-        maxLength: 128
-    },
-    createdAt: {
-        type: Date,
-        default: () => new Date(),
-        get: (date) => {
-            return date.toLocaleDateString('en-US', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit'
-            });
+const reactionSchema = new mongoose.Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: () => new Date(),
+            get: (date) => {
+                return date.toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                });
+            }
         }
     }
-    
-});
+)
+
+const thoughtSchema = new mongoose.Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 128
+        },
+        createdAt: {
+            type: Date,
+            default: () => new Date(),
+            get: (date) => {
+                return date.toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                });
+            }
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [{
+            type: reactionSchema
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+
 
 thoughtSchema
+.virtual('reactionCount')
 .get(function () {
-    return 
+    return this.reactions.length
 })
+
+// thoughtSchema
+// .virtual('formattedDate')
+// .get(function () {
+//     return this.date.toLocaleDateString('en-US', {
+//         year: 'numeric',
+//         month: 'long',
+//         day: 'numeric',
+//         hour: '2-digit',
+//         minute: '2-digit',
+//         second: '2-digit'
+//     });
+// })
 
 
 
